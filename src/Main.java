3e85f7456a48d11558d62e055ae2658529aff2d7
savilin <valkonskyspb@icloud.com>
2022-04-12 02:03:12 +0300

    print "ABC" using many threads

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
-    public static void main(String[] args) {
+    private static final Object obj = new Object();
+    private static volatile char currentLetter = 'A';
+    private Object object = new Object();
 
+    public static void main(String[] args) throws InterruptedException {
+        Main main = new Main();
+        Thread first = new Thread(() -> {
+            main.printA();
+        });
+        Thread second = new Thread(() -> {
+            main.printB();
+        });
+        Thread third = new Thread(() -> {
+            main.printC();
+        });
+        first.start();
+        second.start();
+        third.start();
+    }
+
+    private void printA() {
+        synchronized (obj){
+            try{
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        obj.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter='B';
+                    obj.notifyAll();
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }    private void printB() {
+        synchronized (obj){
+            try{
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        obj.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter='C';
+                    obj.notifyAll();
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }   private void printC() {
+        synchronized (obj){
+            try{
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        obj.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter='A';
+                    obj.notifyAll();
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
